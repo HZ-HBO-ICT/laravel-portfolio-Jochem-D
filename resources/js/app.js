58f8d6aed83550a58c890ffdcbc7cebe62c66f5b
Jochem Dijkdrent <dijk0146@hz.nl>
2022-06-23 13:16:49 +0200

    added api test

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,14 +1,4 @@
 require('./bootstrap');
 
-function getSwapiData() {
-    fetch("https://kitsu.io/api/edge/users/1288763/library-entries")
-        .then((response) => response.json())
-        .then((data) => {
-            const ul = document.createElement("ul");
-            ul.innerHTML = `<li>${data.name}</li>`;
-            domElement.append(ul);
-        })
-        .catch((err) => {
-            console.log("something went wrong", err);
-        });
-}
+
+
